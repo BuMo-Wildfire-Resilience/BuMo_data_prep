ca1523b7d79e1a6a5974b71b24652bff574dbbad
Firstname Lastname <firstname.lastname@gov.bc.ca>
2024-11-14 09:54:00 -0800

    update

--- header.R
+++ header.R
@@ -44,14 +44,14 @@ control.compute=list(save.memory=TRUE)
 #remotes::install_github("bcgov/climr", force=TRUE)
 #library(climr)
 
-#DataDir <- '../../../Bulkley Morice Wildfire Resilience Pilot/Analysis/Data/BuMo_data_prep/data'
-DataDir<-'data'
+DataDir <- '../../../Bulkley Morice Wildfire Resilience Pilot/Analysis/BuMo_data_prep/data'
+#DataDir<-'data'
 dir.create(DataDir, showWarnings = FALSE)
 spatialDir <- file.path(DataDir,'spatial')
 dir.create(spatialDir, showWarnings = FALSE)
 
-#OutDir <- '../../../Bulkley Morice Wildfire Resilience Pilot/Analysis/Data/BuMo_data_prep/out'
-OutDir<-'out'
+OutDir <- '../../../Bulkley Morice Wildfire Resilience Pilot/Analysis/BuMo_data_prep/out'
+#OutDir<-'out'
 dir.create(file.path(OutDir), showWarnings = FALSE)
 dataOutDir <- file.path(OutDir,'data')
 dir.create(file.path(dataOutDir), showWarnings = FALSE)
