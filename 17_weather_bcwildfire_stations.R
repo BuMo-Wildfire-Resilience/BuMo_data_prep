411795b961a255ee451ca7ececdf87da66357e19
gcperk <genevieve.c.perkins@gmail.com>
2025-08-26 15:28:55 -0700

    add documentaion

--- 17_weather_bcwildfire_stations.R
+++ 17_weather_bcwildfire_stations.R
@@ -1,4 +1,15 @@
-# 17_weather_BCwildfire_stations
+## 17_weather_BCwildfire_stations
+
+# this script will format bc data weather station information to generate raster layers for 
+# weather and fire metrics across the Bumo Grid. 
+
+# 1) Download the station meta information from bcdata package 
+# 2) Format and combine weather station data. Relies on manual download of the data priori to this. 
+# 3) For the raster template - determine which raster cell is closest to which station. note this 
+      # has two outputs as station installed in Nov 2018
+# 4) Generate weather data rasters for each time point ie 20140401 and for each metric (i.e FWI) 
+ # by extracting the closest station inforamtion. Note there is a function to do do these. 
+
 
 library(terra)
 library(sf)
@@ -52,8 +63,6 @@ st_write(st_bumo, path(spatialDir, "weather", "BuMo", "bumo_weather_stations.gpk
 # 2) Compile the weather data downloaded from the BCdata site 
 # These were donwloaded for annual years from 2014 - 2023 as annual zip files. 
 
-
-
 # note hourly weather for fwi and fire metrics is available under a data sharing agreement, just starting with daily average to get 
 # code started for ground truthing the data avavilabel and compare with WRF. 
 
@@ -89,12 +98,12 @@ mods <- mods |>
   mutate(ddate = as_date(DATE)) |> 
   mutate(jday = yday(ddate))
 
-# summarise which stations have data 
-
-st_summary <- mods |> 
-  group_by(STATION_CODE) |> 
-  summarise(min(DATE_TIME),
-            max(DATE_TIME))
+## summarise which stations have data 
+# 
+# st_summary <- mods |> 
+#   group_by(STATION_CODE) |> 
+#   summarise(min(DATE_TIME),
+#             max(DATE_TIME))
 
 
 # select FWI variables as these are generated daily at 12noon? 
@@ -231,7 +240,9 @@ writeRaster(post2019, fs::path(spatialDir, "weather", "BuMo", "bumo_post2019_clo
 
 
 
-# Generate the weather data
+# 4) Generate the weather data rasters for each time point and for each metric by extracting
+# the closest station inforamtion. 
+
 all_stat_weather <- read_csv(file = fs::path(spatialDir, "weather", "BuMo", "bumo_weather_daily_20142023.csv")) |> 
   select(-DATE_TIME) |> 
   mutate(month = month(ddate)) |> 
@@ -454,152 +465,6 @@ fwi_2014 <- generate_weather_raster(
 
 #fwi_2014 <- fwi_2014_2018
 
-
-
-
-#############################################################################################################3
-
-# calculate the FWI using the CSSF tutorial
-
-#remotes::install_github("cffdrs/cffdrs_r")
-library(cffdrs)
-library(lubridate)
-library(data.table)
-library(lutz)
-library(readr)
-library(sf)
-
-# load in helper functions... not sure why these are not part of the package? Note these have
-# been manually moved and copied on August 18th as they were not accessible within the package directly 
-
-source("17_csffdr_utils.R")
-
-st_bumo <- st_read( fs::path(spatialDir, "weather", "BuMo", "bumo_weather_stations.gpkg")) |> 
-  select(STATION_NAME, LATITUDE, LONGITUDE) |> 
-  st_drop_geometry()
-
-# id, lat, long, yr, mon, day, hr, temp, rh, ws, prec
-
-# lets test this using the bc data stations # test on one station
-
-wtest <- read_csv(fs::path(spatialDir, "weather", "BuMo", "bumo_weather_obs_20142023.csv"))
-
-# join the lat and long cols
-wtest <- left_join(wtest, st_bumo)
-
-wtest <- wtest |> 
-  #filter(STATION_NAME == "HOUSTON") |> 
-  rename(
-    "id" = STATION_CODE,
-    "lat" = LATITUDE,
-    "long" = LONGITUDE,
-    "temp" = HOURLY_TEMPERATURE ,
-    "rh" = HOURLY_RELATIVE_HUMIDITY,
-    "ws" = HOURLY_WIND_SPEED ,
-    "prec" = HOURLY_PRECIPITATION 
-  ) 
-
-wtest <- wtest |> 
-  mutate(yr = as.numeric(substr(DATE_TIME, 1, 4)),
-         mon = as.numeric(substr(DATE_TIME, 5,6)),
-         day = as.numeric(substr(DATE_TIME, 7,8)),
-         hr = as.numeric(substr(DATE_TIME, 9,10))) 
-
-wtest <- wtest |> select(id, lat, long, yr, mon, day, hr, temp, rh, ws, prec,DATE_TIME)
-wtest <- wtest |> 
-  filter(mon %in% c(4,5,6,7,8,9,10))
-
-data <- wtest
-#wtest = NA
-
-# checks for weather data ################
-# 1) RH 
-# replace rh = NA with rh = 0
-data <- data %>%
-  mutate(rh = ifelse(is.na(rh), 0, rh))
-
-# 2) WS
-data <- data %>%
-  mutate(ws = ifelse(is.na(ws), 0, ws))
-
-# 3) prec
-data <- data %>%
-  mutate(prec = ifelse(is.na(prec), 0, prec))
-
-
-# Print the column names, data should contain the following 11 columns:
-# id, lat, long, yr, mon, day, hr, temp, rh, ws, prec
-names(data)
-
-### Find the timezone ###
-# The 'lutz' library has functions to get the timezone of the weather station
-# based on latitude and longitude. First, make a dataframe of stations with
-# unique ID, latitude, and longitude.
-stations <- unique(data[c("id", "lat", "long")])
-
-# Print the unique station IDs and coordinates. For this dataset the only station
-# is at Petawawa Research Forest (PRF).
-stations
-
-
-# Find the timezone based on latitude and longitude, this can take some time.
-# You may need to download the package 'sf' for method = "accurate".
-tz_loc <- tz_lookup_coords(stations$lat, stations$long, method = "accurate")
-# Print the timezone location. PRF is equivalent to "America/Toronto".
-tz_loc <- unique(tz_loc)
-
-utc <- tz_offset("2014-01-01", tz_loc)[[5]]
-utc
-# 
-# 
-# # temporarty check a subset 
-# nas <- data |> filter(!is.na(rh))
-# 
-# data <- data |> 
-#   filter(!is.na(rh)) |> 
-#   filter(!is.na(ws)) |> 
-#   filter(!is.na(prec)) 
-
-### Calculate hourly FWI System outputs with FWI2025 ###
-# hFWI() is the function that calculates hourly FWI codes in FWI2025. It can
-# handle multiple stations and years/fire seasons (not shown in this tutorial).
-# Make sure to specify the corresponding UTC offsets for different stations.
-# Default starting FWI codes are: ffmc_old = 85, dmc_old = 6, dc_old = 15
-
-
-
-data2014 <- data |> 
-  filter(yr == 2014) |> 
-  filter(id ==105)
-
-# check that the difference in hr from one row to the next row is only 1 hour using lag
- data2014 <- data2014 |>
-  mutate(hr_diff = hr - lag(hr)) 
-
-
-
-#select(id, lat, long, yr, mon, day, hr, temp, rh, ws, prec)
-
-data_fwi <- hFWI(data, utc)
-
-# Output is a data TABLE, with FWI calculations appended after the input columns.
-# Save the output as a .csv file (overrides any data in any preexisting file).
-write.csv(data_fwi, "wx_prf_fwi.csv")
-
-# View a simple summary of the standard FWI components.
-standard_components <- c("ffmc", "dmc", "dc", "isi", "bui", "fwi")
-View(summary(data_fwi[, ..standard_components]))
-
-### Calculate daily summaries ###
-# Calculate outputs like peak burn time and number of hours of spread potential.
-report <- generate_daily_summaries(data_fwi)
-
-# View a simple summary of the daily report (convert values to numeric class first).
-daily_components <- c("peak_time", "duration", "peak_isi_smoothed", "dsr")
-View(summary(apply(report[daily_components], 2, as.numeric)))
-
-# From here, the outputs can be converted to any datatype for further analysis or
-# plotted for visualization.
 
 
 
