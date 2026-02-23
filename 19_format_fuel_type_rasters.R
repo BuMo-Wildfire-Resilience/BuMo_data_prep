80b4793e31373ecda3cfca69292513b9bd3a3bff
gcperk <genevieve.c.perkins@gmail.com>
2026-02-23 11:07:32 -0800

    update fuel type data

--- 19_format_fuel_type_rasters.R
+++ 19_format_fuel_type_rasters.R
@@ -27,7 +27,7 @@ raster_template[raster_template ==1]<- 0
 
 ft17 <-  rast(path(spatialDir, "FM_FUEL_TYPE_GRID_2017", "FM_FUEL_TYPE_GRID_2017.tif"))
 ft17 <- crop(ft17, raster_template)
-ft17p <- project(ft17,raster_template )
+ft17p <- project(ft17,raster_template, method = "mode")
 names(ft17p)<- "FT_2017"
 
 #ft_all <- c(ft17p, raster_template)
@@ -35,17 +35,16 @@ names(ft17p)<- "FT_2017"
 
 ft18 <-  rast(path(spatialDir, "FM_FUEL_TYPE_GRID_2018", "FM_FUEL_TYPE_GRID_2018.tif"))
 ft18 <- crop(ft18, raster_template)
-ft18p <- project(ft18,raster_template )
+ft18p <- project(ft18,raster_template , method = "mode")
 names(ft18p)<- "FT_2018"
 
 #ft_all <- c(ft17p, ft18p, raster_template)
 
 # format the 2020 - 2025 fuel types 
 
-
 ft20 <- rast(fs::path(DataDir,'spatial', "Fuel_types_BC", "Hist_FT_Rasters.gdb"))
 ft20 <- crop(ft20, raster_template)
-ft20 <- project(ft20,raster_template )
+ft20 <- project(ft20,raster_template , method = "mode")
 
 ft20p <- c(ft17p, ft18p,ft20 )
 ft20p <- ft20p[[-3]]
@@ -54,4 +53,4 @@ ordered_names <- c("FT_2017", "FT_2018", "FT_2020", "FT_2022", "FT_2023", "FT_20
 ordered_stack <- ft20p[[ordered_names]]
 
 
-terra::writeRaster(ordered_stack, fs::path(out_dir, "ft_bumo_2017_20204.tif"))
+terra::writeRaster(ordered_stack, fs::path(out_dir, "ft_bumo_2017_20204_mode.tif"))
