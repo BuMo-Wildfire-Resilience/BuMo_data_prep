dade9520f3eb8e9fff12d358bf5b1c9e9430e22a
DonMorgan <dmorgan.bee3@gmail.com>
2026-06-30 15:38:24 -0700

    update

--- 20_conservation.R
+++ 20_conservation.R
@@ -5,23 +5,10 @@ AOI_50km<-st_read(file.path(spatialOutDir,'AOI_50km.gpkg'))
 #Transmission Lines
 TransmissionLines<-bcdc_get_data("WHSE_BASEMAPPING.GBA_TRANSMISSION_LINES_SP") %>%
   st_intersection(AOI_50km)
+write_sf(TransmissionLines, file.path(spatialOutDir,"TransmissionLines.gpkg"))
 #Pipelines
+Pipelines<-bcdc_get_data("WHSE_MINERAL_TENURE.OG_PIPELINE_AREA_PERMIT_SP") %>%
+  st_intersection(AOI_50km) %>%
+  dplyr::filter(LAND_STAGE_DESC=='Post Construction Plan')
+write_sf(Pipelines, file.path(spatialOutDir,"Pipelines.gpkg"))
 
-  
-write_sf(KBLUP_ConnectNonLegal, file.path(spatialOutDir,"KBLUP_ConnectNonLegal.gpkg"))
-
-Transmission <- st_read(file.path(spatialDir, "WHSE_BASEMAPPING.GBA_TRANSMISSION_LINES_SP.gpkg"))
-
-disturbance_sfR1<-st_read(file.path(spatialOutDir,'disturbance_sfR1.gpkg'))
-BCrBuff<-rast(file.path(spatialOutDir,'BCrBuff.tif'))
-#Transmission Lines
-P_transmission<-disturbance_sfR1 %>%
-  dplyr::filter(disturb=='Transmission') %>%
-  st_buffer(dist=50) %>%
-  mutate(transmission=1) %>%
-  dplyr::select(transmission)
-P_transmissionR<-rasterize(vect(P_transmission), ProvRastB, field="transmission") %>%
-  crop(BCrBuff, mask=TRUE)
-writeRaster(P_transmissionR, file.path(spatialOutDir,'P_transmissionR.tif'), overwrite=TRUE)
-#Join Extra Provincial data to Provincial data
-EP_transmissionR<-rast(file.path(spatialOutDir,'P_transmissionR.tif'))
